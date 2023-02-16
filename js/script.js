b08ee97482cab9fa82fb9539756660ee12b2fbc1
effiongcharles <talkwithcharles@gmail.com>
2023-02-16 19:24:19 +0100

    show/hide header implementation on scroll

--- js/script.js
+++ js/script.js
@@ -1,28 +1,14 @@
 let mybutton = document.getElementById("btn-top");
 
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
 function openNav() {
+  document.getElementById("header-desktop").style.display = "none";
   document.getElementById("mobile-nav").style.width = "250px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.5)";
+  getElementById("contact").style.pointerEvents = "none";
 }
 
 function closeNav() {
+  document.getElementById("header-desktop").style.display = "flex";
   document.getElementById("mobile-nav").style.width = "0";
   document.body.style.backgroundColor = "white";
 }
@@ -199,3 +185,48 @@ window.onload = function () {
         });
     });
 };
+
+var prevScrollPos =
+  document.documentElement.scrollTop || document.body.scrollTop;
+
+//scrolling
+
+//show or hide "scroll to top button"
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+//scroll to top
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+document.addEventListener(
+  "scroll",
+  (event) => {
+    document.getElementById("header-mobile").display = "none";
+    scrollFunction();
+    // handle scroll event
+    var currScrollPosition =
+      window.pageYOffset || document.documentElement.scrollTop;
+
+    document.documentElement.scrollTop || document.body.scrollTop;
+    if (currScrollPosition > prevScrollPos) {
+      //scrolling down
+      document.getElementById("header-desktop").style.transform =
+        "translateY(-200px)";
+    } else if (currScrollPosition < prevScrollPos) {
+      //scrolling up
+      document.getElementById("header-desktop").style.transform =
+        "translateY(0)";
+    }
+
+    prevScrollPos = currScrollPosition;
+  },
+  { passive: true }
+);
